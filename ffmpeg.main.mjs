4104f8c0b1f1da470519ced4a385ae46d87b619c
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2023-06-16 06:15:16 +0200

    renamed main entry and moved array .flat to polyfills directory.

--- ffmpeg.main.mjs
+++ ffmpeg.main.mjs
@@ -1,21 +1,5 @@
 import fs from "file-system-es6";
-
-function flat(array) {
-  if (typeof array !== "object") {
-    return array;
-  } else
-    for (let index in array) {
-      const entry = array[index];
-      if (entry && entry.constructor && entry.constructor.name === "Array") {
-        for (let value of entry) {
-          array.push(value);
-        }
-        array.splice(index, 1);
-        array = flat(array);
-      }
-    }
-  return array;
-}
+import flat from "./polyfills/array-flat.mjs";
 
 Array.prototype.flat = function () {
   return flat(this[0]);
